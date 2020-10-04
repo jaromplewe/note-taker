f310b17f679ce65039d27958fd757c582295bd80
Jarom Plewe <jplewe23@gmail.com>
2020-10-04 16:01:57 -0600

    express routing for notes page works.

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 })
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 })
+
+
+app.listen(PORT, () => {
+    console.log('listening on port' + PORT);
+})
+
